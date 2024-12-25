13822ad15541541fff8bb9841536045474554ac8
Kavindu Wijegunasekara <110486479+Kavinduprabasara@users.noreply.github.com>
2024-12-25 13:33:43 +0530

    fix: camera angle fixed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject Player;
-    public Vector3 offset = new Vector3(5, 5, -11);
+    public Vector3 offset = new Vector3(0, 6, -8);
     // Start is called before the first frame update
     void Start()
     {
