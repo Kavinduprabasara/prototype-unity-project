1f33604721085540df1e0186a6c75f818a407514
Kavindu Wijegunasekara <110486479+Kavinduprabasara@users.noreply.github.com>
2024-12-25 18:17:36 +0530

    action check

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject Player;
-    public Vector3 offset = new Vector3(0, 6, -8);
+    public Vector3 offset = new Vector3(0, 6, -9);
     // Start is called before the first frame update
     void Start()
     {
